809e0c01c19184b2744a64532e5a66331486cdd5
Jolisa Tran <jolisa.chan.store@gmail.com>
2020-04-19 23:45:56 -0400

    add logger and context bean in main

--- core/src/main/java/org/springlearning/AppMain.java
+++ core/src/main/java/org/springlearning/AppMain.java
@@ -1,7 +1,20 @@
 package org.springlearning;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class AppMain {
-    public static void main(String[] args){
-        System.out.println("Hello");
+    private static final Logger log = LoggerFactory.getLogger(AppMain.class);
+    private static final String CONFIG_LOCATION = "beans.xml";
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+        int number = numberGenerator.next();
+        context.close();
+
     }
 }
